8723ed6dff86c3253c9b11af08998bf6e290ec0d
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-26 21:32:57 -0400

    props 2 JSX syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-var path = require('path');
-
 module.exports = {
+
     mode: 'development',
-    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    entry: `${__dirname}/src/index.js`,
     output: {
-        path: path.resolve(__dirname, 'dist') + '/app',
+        path: `${__dirname}/dist/app`,
         filename: 'bundle.js',
         publicPath: '/app/'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
+                include: `${__dirname}/src`,
                 loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015']
