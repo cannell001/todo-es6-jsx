7f1cdba74c82bcf5453e0a66f4cfdd5eb9e5cf88
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-28 10:28:29 -0400

    State 2 JSX map function

--- src/index.js
+++ src/index.js
@@ -6,20 +6,22 @@ export default class TodoComponent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            todos: ['wash the dishes', 'cut the cheese', 'take a nap']
+            todos: ['drink tea', 'wash the dishes', 'cut the cheese', 'take a nap']
         };
     }
     //getInitialState
     render(){
+		var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(
+                    <li>{item}</li>
+            );
+        });
         return(
             <div id="todo-list">
-				<h2>State with JSX</h2>
+				<h2>State 2 w/ JSX</h2>
                 <p>React stuff goes here...</p>
-				<ul>
-					<li>{this.state.todos[0]}</li>
-					<li>{this.state.todos[1]}</li>
-					<li>{this.state.todos[2]}</li>
-				</ul>
+				<ul>{todos}</ul>
             </div>
         );
     } //render
