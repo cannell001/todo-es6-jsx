760f9c71d5f0a9669237b559e10448b3226607fd
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-28 11:04:58 -0400

    Separate TodoItem Component - JSX

--- src/index.js
+++ src/index.js
@@ -6,25 +6,37 @@ export default class TodoComponent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            todos: ['drink tea', 'wash the dishes', 'cut the cheese', 'take a nap']
+            todos: ['drink tea', 'put out garbage', 'walk the dog', 'get a coffee', 'wash the dishes', 'cut the cheese', 'take a nap']
         };
     }
     //getInitialState
     render(){
 		var todos = this.state.todos;
-        todos = todos.map(function(item, index){
-            return(
-                    <li>{item}</li>
-            );
-        });
-        return(
+        todos = todos.map((item, index) => {
+				return(<TodoItem key={index} item={item}/>);
+		});
+
+		return(
             <div id="todo-list">
-				<h2>State 2 w/ JSX</h2>
-                <p>React stuff goes here...</p>
+				<h2>Separate TodoItem Component w/ JSX</h2>
+                <p>Output the TodoItems...</p>
 				<ul>{todos}</ul>
             </div>
         );
     } //render
+}
+
+//Create TodoItem component
+class TodoItem extends Component {
+	render(){
+		return(
+			<li>
+                <div className="todo-item">
+                    <span className="item-name">{this.props.item}</span>
+                </div>
+            </li>
+		);
+	}
 }
 
 ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
