905b74b7e15899405b73112cf8735ebe07c3395a
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-02 12:23:26 -0400

    OnDelete Event Handler

--- index.js
+++ index.js
@@ -5,31 +5,21 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
     getInitialState: function(){
         return {
-            todos: ['wash up', 'eat some cheese', 'take a nap']
+            todos: ['drink tea', 'wash the dishes', 'cut the cheese', 'take a nap']
         }
     }, //getInitialState
     render: function(){
         var todos = this.state.todos;
         todos = todos.map(function(item, index){
-            return(<TodoItem key={index} item={item} onDelete={this.onDelete} />);
-        }.bind(this));
+            return(<TodoItem key={index} item={item} />);
+        });
         return(
             <div id="todo-list">
                 <p>The busiest people have the most leisure...</p>
                 <ul>{todos}</ul>
             </div>
         );
-    }, //render
-
-    //Custom functions
-    onDelete: function(item){
-        var updatedTodos = this.state.todos.filter(function(val, index){
-            return item !== val;
-        });
-        this.setState({
-          todos: updatedTodos
-        });
-    }
+    } //render
 });
 
 //Create TodoItem component
@@ -39,15 +29,9 @@ var TodoItem = React.createClass({
             <li>
                 <div className="todo-item">
                     <span className="item-name">{this.props.item}</span>
-                    <span className="item-remove" onClick={this.handleDelete}> x </span>
                 </div>
             </li>
         );
-    },
-
-    //Custom functions
-    handleDelete: function(){
-        this.props.onDelete(this.props.item);
     }
 });
 
