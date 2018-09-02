8cd43a018a685ef6b61a2e91acf50a59542223e5
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-02 11:44:10 -0400

    Props 2 React es6 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    entry: path.resolve(__dirname, 'src') + '/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist') + '/app',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/app/'
+        publicPath: '/'
     },
     module: {
         loaders: [
