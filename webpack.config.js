5ff4b613ba058475583684c8be5aede1b102c7c1
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-05 14:08:06 -0400

    Fixed Webpack Config Update Node Modules

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 include: `${__dirname}/src`,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'env']
                 }
             },
             {
